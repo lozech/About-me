2778b3980846588b1eccc38d59945f086ffde76e
lozech <thfgk1122@gmail.com>
2026-04-01 11:14:35 +0900

    fix2

--- js/main.js
+++ js/main.js
@@ -11,21 +11,37 @@ whiteBtn.addEventListener("click", () => {
 
 
 const indexPage = document.querySelector("#index-page");
+const lastPage = document.querySelector("#last-page");
 
 const observer = new IntersectionObserver(
-    ([entry]) => {
-        if (entry.isIntersecting) {
-        document.body.classList.add("index");
-        } else {
-        document.body.classList.remove("index");
-        }
+    (entries) => {
+        entries.forEach((entry) => {
+            if (entry.target.id === "index-page") {
+                if (entry.isIntersecting) {
+                    document.body.classList.add("index");
+                } else {
+                    document.body.classList.remove("index");
+                }
+            }
+
+            if (entry.target.id === "last-page") {
+                if (entry.isIntersecting) {
+                    document.body.classList.add("last");
+                } else {
+                    document.body.classList.remove("last");
+                }
+            }
+        });
     },
     {
-        threshold: 0.6 // index가 60% 이상 보일 때만 숨김
+        threshold: 0.6
     }
 );
 
 observer.observe(indexPage);
+observer.observe(lastPage);
+
+
 
 const uiSections = document.querySelectorAll(".ui-section");
 const contentSections = document.querySelectorAll(".content-section");
@@ -79,12 +95,14 @@ function createFirstUISection(type, title, leftImg, rightImg) {
     return `
         <div class="ui-inner">
             <p class="ui-title">UI Design</p>
+            <p class="ui-desc2">Skill : Photoshop</p>
             <p class="ui-desc">
                 “ 클래식부터 키치한 디자인까지, 다양한 컨셉을 시도하며 표현의 폭을 넓혀가고 있습니다. ” 
             </p>
+            
 
             <div class="ui-sub">
-                <p class="type">Type ${type}.</p>
+                <p class="type">Concept Type ${type}.</p>
                 <p class="type-title">${title}</p>
             </div>
 
@@ -102,7 +120,7 @@ function createUISection(type, title, leftImg, rightImg) {
             <p class="ui-title">UI Design</p>
 
             <div class="ui-sub">
-                <p class="type">Type ${type}.</p>
+                <p class="type">Concept Type ${type}.</p>
                 <p class="type-title">${title}</p>
             </div>
 
@@ -155,22 +173,25 @@ function createContentSection(title, leftImg, rightImg, link, isFirst, desc1, de
                         : ""
                 }
             </div>
+                    
+            <div class="content-wrap">
+                <div class="content-book">
+                    <img src="${leftImg}" alt="">
+                    ${rightImg ? `<img src="${rightImg}" alt="">` : ""}
+                </div>
 
-            <div class="content-book">
-                <img src="${leftImg}" alt="">
-                ${rightImg ? `<img src="${rightImg}" alt="">` : ""}
+                <div class = "desc-wrap">
+                    ${desc1?.trim() ? `<p class="content-img-desc"><span class="tits">Skills : </span> <span class="skill">${desc1}</span></p>` : ""}
+                    ${desc2?.trim() ? `<p class="content-img-desc"><span class="tits">만든 페이지 : </span> ${desc2}</p>` : ""}
+                    ${desc3?.trim() ? `
+                        <p class="content-img-desc2">
+                            <span class="tits">기획의도 :</span>
+                            <span class="desc-text">${desc3}</span>
+                        </p>` : ""
+                    }
+                </div>
             </div>
-
-            <div class = "desc-wrap">
-                ${desc1?.trim() ? `<p class="content-img-desc"><span class="tits">기여도 : </span> ${desc1}</p>` : ""}
-                ${desc2?.trim() ? `<p class="content-img-desc"><span class="tits">만든 페이지 : </span> ${desc2}</p>` : ""}
-                ${desc3?.trim() ? `
-                    <p class="content-img-desc2">
-                        <span class="tits">기획의도 :</span>
-                        <span class="desc-text">${desc3}</span>
-                    </p>` : ""
-                }
-            </div>
+            
         </div>
         `;
 }
@@ -180,13 +201,23 @@ const sections2 = document.querySelectorAll(".content-section");
 const data2 = [
     [
         1, "soopsori", "./images/soopsori.png", null, "https://lozech.github.io/soopsori/",
-        "80%","메인", "숲소리는 자연의 공간을 구현하는 키즈 가구 브랜드입니다. 코딩을 처음 시작하며 제작한 페이지로, 환경과 아이들을 고려한 UX를 구현하고자 했습니다. 메인 페이지 구조와 퀵메뉴, 서브메뉴 등 기본적인 기능을 처음으로 직접 구현해보며 구조를 이해한 작업입니다."
+        "HTML5/CSS3/JAVA Script","메인", "숲소리는 자연의 공간을 구현하는 키즈 가구 브랜드입니다. 코딩을 처음 시작하며 제작한 페이지로, 환경과 아이들을 고려한 UX를 구현하고자 했습니다. 메인 페이지 구조와 퀵메뉴, 서브메뉴 등 기본적인 기능을 처음으로 직접 구현해보며 구조를 이해한 작업입니다."
     ],
-    [2, "MAMMOTH COFFEE", "./images/mmth.png", null, "https://lozech.github.io/mmth/","80%","메인", "합리적인 가격에 양질의 커피를 추구하는 커피브랜드 매머드커피입니다. 메인배너를 영상으로 채운 작업이 처음이었으며, 홍보적인 부분이 두각될 수 있도록 여러 애니메이션 효과들이 돋보일 수 있는 UI를 진행해보았습니다."],
-    [3, "oth", "./images/oth.png", null, "https://lozech.github.io/oth/","100%","메인", "일상과 여행에서 얻은 영감을 오감으로 풀어내는 브랜드 오티에이치콤마(Oth,), 압화도구/독서링/무드등과 같이 인테리어 소품을 판매하는 브랜드인만큼 감각적인 부분이 잘 드러나는 것에 집중한 프로젝트였습니다."],
-    [4, "Ably-event", "./images/ably-event1.png", "./images/ably-event2.png", "https://lozech.github.io/ably/","80%","이벤트페이지", "처음으로 진행해본 팀프로젝트로서 에이블리 쇼핑몰 브랜드를 리뉴얼 해봤습니다. 기존 사이트의 PC버전 부재로 인해 리뉴얼 대상으로 선정하였습니다. 기존에 가독성이 떨어지던 이벤트 배너들을 상세페이지로 제작하며 기획 감각을 키워가고자 했습니다. 또한 의류브랜드로서의 감각을 부각하기 위해 HTML, CSS 중심 작업에서 나아가 JQuery를 활용해 인터랙션 기능을 직접 구현해봤습니다."],
-    [5, "Something Play", "./images/something.png", null, "https://lozech.github.io/somethingplay/","100%","메인", "파주 헤이리마을에 위치한 소품샵 브랜드, 기존 사이트의 파비콘과 로고를 감각적으로 재디자인해보며 주도적으로 기획하고자 했던 첫번째 사이트 입니다. 자바스크립트를 활용하여 캐러셀 기능도 구현하며 부가요소들을 더 활용해보고자 했습니다."],
-    [6, "JO'S LOUNGE", "./images/jo's.png", null, "https://lozech.github.io/jo-s/","100%","메인", "계절에 어울리는 가벼운 소재를 사용하여 편안한 공간을 구성하게 도와주는 잠옷 브랜드, 기존 사이트의 클래식한 감각을 살리기 위해 필요한 부분에 집중하였습니다. 큰 전환율 변화를 일으키지 않는 부분은 간추리고 가독성을 높이는 부분에 집중하여 리뉴얼을 진행했습니다."],
+    [
+        2, "MAMMOTH COFFEE", "./images/mmth.png", null, "https://lozech.github.io/mmth/",
+        "HTML5/CSS3/JAVA Script","메인", "합리적인 가격에 양질의 커피를 추구하는 커피브랜드 매머드커피입니다. 메인배너를 영상으로 채운 작업이 처음이었으며, 홍보적인 부분이 두각될 수 있도록 여러 애니메이션 효과들이 돋보일 수 있는 UI를 진행해보았습니다."],
+    [
+        3, "oth", "./images/oth.png", null, "https://lozech.github.io/oth/",
+        "HTML5/CSS3","메인", "일상과 여행에서 얻은 영감을 오감으로 풀어내는 브랜드 오티에이치콤마(Oth,), 압화도구/독서링/무드등과 같이 인테리어 소품을 판매하는 브랜드인만큼 감각적인 부분이 잘 드러나는 것에 집중한 프로젝트였습니다."],
+    [
+        4, "Ably-event", "./images/ably-event1.png", "./images/ably-event2.png", "https://lozech.github.io/ably/",
+        "HTML5/CSS3/JQuery","이벤트페이지", "처음으로 진행해본 팀프로젝트로서 에이블리 쇼핑몰 브랜드를 리뉴얼 해봤습니다. 기존 사이트의 PC버전 부재로 인해 리뉴얼 대상으로 선정하였습니다. 기존에 가독성이 떨어지던 이벤트 배너들을 상세페이지로 제작하며 기획 감각을 키워가고자 했습니다. 또한 의류브랜드로서의 감각을 부각하기 위해 HTML, CSS 중심 작업에서 나아가 JQuery를 활용해 인터랙션 기능을 직접 구현해봤습니다."],
+    [
+        5, "Something Play", "./images/something.png", null, "https://lozech.github.io/somethingplay/",
+        "HTML5/CSS3/JQuery","메인", "파주 헤이리마을에 위치한 소품샵 브랜드, 기존 사이트의 파비콘과 로고를 감각적으로 재디자인해보며 주도적으로 기획하고자 했던 첫번째 사이트 입니다. 자바스크립트를 활용하여 캐러셀 기능도 구현하며 부가요소들을 더 활용해보고자 했습니다."],
+    [
+        6, "JO'S LOUNGE", "./images/jo's.png", null, "https://lozech.github.io/jo-s/",
+        "HTML5/CSS3/JQuery","메인", "계절에 어울리는 가벼운 소재를 사용하여 편안한 공간을 구성하게 도와주는 잠옷 브랜드, 기존 사이트의 클래식한 감각을 살리기 위해 필요한 부분에 집중하였습니다. 큰 전환율 변화를 일으키지 않는 부분은 간추리고 가독성을 높이는 부분에 집중하여 리뉴얼을 진행했습니다."],
 ];
 
 sections2.forEach((section2, i) => {
@@ -238,7 +269,7 @@ function createWebSection(title, leftImg, rightImg, link, isFirst, desc1, desc2,
             </div>
 
             <div class = "web-wrap">
-                ${desc1?.trim() ? `<p class="web-img-desc"><span class="tits">기여도 : </span> ${desc1}</p>` : ""}
+                ${desc1?.trim() ? `<p class="web-img-desc"><span class="tits">Skills : </span> <span class="skill">${desc1}</span></p>` : ""}
                 ${desc2?.trim() ? `<p class="web-img-desc"><span class="tits">만든 페이지 : </span> ${desc2}</p>` : ""}
                 ${desc3?.trim() ? `
                     <p class="web-img-desc2">
@@ -256,11 +287,16 @@ const sections3 = document.querySelectorAll(".web-section");
 const data3 = [
     [
         1, "More Vision", "./images/morevision.png", null,"https://lozech.github.io/More-Vision/",
-        "100%","메인", "모어비전은 래퍼 박재범이 장르 문화를 기반으로 설립한 새로운 엔터테인먼트 허브입니다. 처음으로 swiper.js와 gsap을 활용하여 사이트 전체를 리뉴얼 해보았습니다."
+        "JAVA Script/Swiper.js/GSAP","메인(전 페이지 구현)", "모어비전은 래퍼 박재범이 장르 문화를 기반으로 설립한 새로운 엔터테인먼트 허브입니다. 처음으로 swiper.js와 gsap을 활용하여 사이트 전체를 리뉴얼 해보았습니다."
     ],
-    [2, "EMET SOUND","./images/emet2.png", null, "https://lozech.github.io/emet-sound/","100%","메인/ABOUT/ARTISTS/LABEL/NEWS", "에메트사운드 레이블은 장르의 경계를 허물고 '에메트스러움'을 음악으로 치환하는 사운드 프로듀싱 허브입니다. 리액트 라이브러리를 사용하여 진행한 프로젝트로서 사이트 구현에 있어서 속도를 높이고자 하였습니다. 링크 간의 이동이 자연스럽고, 컴포넌트 활용이 자유로운 리액트를 통해 하나의 사이트를 더욱 더 효과적으로 완성할 수 있었습니다."],
-    [3, "Beyond Sangsang", "./images/beyond2.png", null, "https://lozech.github.io/Beyond-Sangsang/","100%","메인/ABOUT/WORKS/RECIPE/CREW/CONTACT", "비욘드상상은 작가와 프로듀서진들로 구성된 광고 제작사입니다. 리액트로 진행한 두번째 프로젝트로서 해당 브랜드에서 추구하는 시선을 끄는 그림을 만드는 이야기에 매력을 느끼고 프로젝트 대상으로 선정하였습니다. 기존 사이트에서 사진과 영상이 로딩이 느린 점을 개선하며 완성도를 높였습니다. 애니메이션 효과를 심층적으로 활용하여 동적인 요소들을 확장하고자 했습니다."],
-    [4, "Blackpaper", "./images/blackpaper.png", null, "https://lozech.github.io/blackpaper/","100%","메인/ABOUT/PROGRAMS/CREATOR/CONTACT", "블랙페이퍼는 방송인 유병재 등이 설립한 크리에이티브 아티스트 매니지먼트사입니다. 리액트로 구현한 세번째 프로젝트로서 블랙페이퍼만의 색깔을 담기 위해 테마별 파비콘을 지정하고 블랙모드/화이트 모드 토글도 제작해보았습니다. 기존 사이트 내 미비한 링크 부분도 보충하여 보다 완성도 있는 리뉴얼을 진행해보았습니다."],
+    [
+        2, "EMET SOUND","./images/emet2.png", null, "https://lozech.github.io/emet-sound/",
+        "REACT/JAVA Script/GSAP","전체 페이지", "에메트사운드 레이블은 장르의 경계를 허물고 '에메트스러움'을 음악으로 치환하는 사운드 프로듀싱 허브입니다. 리액트 라이브러리를 사용하여 진행한 프로젝트로서 사이트 구현에 있어서 속도를 높이고자 하였습니다. 링크 간의 이동이 자연스럽고, 컴포넌트 활용이 자유로운 리액트를 통해 하나의 사이트를 더욱 더 효과적으로 완성할 수 있었습니다."],
+    [
+        3, "Beyond Sangsang", "./images/beyond2.png", null, "https://lozech.github.io/Beyond-Sangsang/",
+        "REACT/JAVA Script","전체 페이지", "비욘드상상은 작가와 프로듀서진들로 구성된 광고 제작사입니다. 리액트로 진행한 두번째 프로젝트로서 해당 브랜드에서 추구하는 시선을 끄는 그림을 만드는 이야기에 매력을 느끼고 프로젝트 대상으로 선정하였습니다. 기존 사이트에서 사진과 영상이 로딩이 느린 점을 개선하며 완성도를 높였습니다. 애니메이션 효과를 심층적으로 활용하여 동적인 요소들을 확장하고자 했습니다."],
+    [4, "Blackpaper", "./images/blackpaper.png", null, "https://lozech.github.io/blackpaper/",
+        "REACT/JAVA Script","전체 페이지", "블랙페이퍼는 방송인 유병재 등이 설립한 크리에이티브 아티스트 매니지먼트사입니다. 리액트로 구현한 세번째 프로젝트로서 블랙페이퍼만의 색깔을 담기 위해 테마별 파비콘을 지정하고 블랙모드/화이트 모드 토글도 제작해보았습니다. 기존 사이트 내 미비한 링크 부분도 보충하여 보다 완성도 있는 리뉴얼을 진행해보았습니다."],
 ];
 
 sections3.forEach((section3, i) => {
